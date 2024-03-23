835ba41b4cac904884d2ffad93444f59edaad05d
basim jawad <basemjm998@gmail.com>
2024-03-24 01:11:00 +0300

    solve schedule error

--- server.js
+++ server.js
@@ -173,31 +173,33 @@ messages.then((message) => {
           `number is ${number.phoneNum} and id is ${number.phoneNumId}`
         );
         cron.schedule("*/2 * * * *", () => {
-          axios({
-            method: "POST",
-            url:
-              `https://graph.facebook.com/v13.0/${number.phoneNumId}/messages?access_token=` +
-              token,
-            data: {
-              messaging_product: "whatsapp",
-              to: `${number.phoneNum}`,
-              text: {
-                body: "Hi Please send your update",
-              },
-            },
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }).then((response) => {
-            console.log(
-              `response is ${JSON.stringify(response.data, null, 2)}`
-            );
-            const messageId = response.data.messages[0].id;
-            const receiverId = response.data.contacts[0].wa_id;
-            saveMessageId(messageId, receiverId);
-          });
+          snedReminderMessage(number);
         });
       });
     }
   });
 });
+
+function snedReminderMessage(number) {
+  axios({
+    method: "POST",
+    url:
+      `https://graph.facebook.com/v13.0/${number.phoneNumId}/messages?access_token=` +
+      token,
+    data: {
+      messaging_product: "whatsapp",
+      to: `${number.phoneNum}`,
+      text: {
+        body: "Hi Please send your update",
+      },
+    },
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    console.log(`response is ${JSON.stringify(response.data, null, 2)}`);
+    const messageId = response.data.messages[0].id;
+    const receiverId = response.data.contacts[0].wa_id;
+    saveMessageId(messageId, receiverId);
+  });
+}
