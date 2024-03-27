bb09d1c44bc5e2c1d7db9877ecba991f3e44a837
basim jawad <basemjm998@gmail.com>
2024-03-27 03:34:10 +0300

    solve payment error

--- server.js
+++ server.js
@@ -22,116 +22,6 @@ app.listen(process.env.PORT || 5000, () => {
 
 app.use("/webhook", webhookRoute);
 
-// get all cutomers in strip dashboard
-// (async () => {
-//   const customers = await stripe.customers.list();
-//   console.log(customers.data);
-// })();
-
-// get spepcific cutomers in strip dashboard
-// (async () => {
-//   const customers = await stripe.customers.retrieve("cus_PnzKlN5Mkg484A", {
-//     // you can add stripe account this admin account in stripe so you can get customer with connected account
-//     stripeAccount: "acct_1OyMbA1D08DtjSM8",
-//   });
-//   console.log(customers);
-// })();
-
-/**
-
-
-//create user with nested parameter
-// (async () => {
-//   try {
-//     const customers = await stripe.customers.create({
-//       email: "khakel@test.com",
-//       name: "jamal",
-//       payment_method: "pm_1Oyhd71D08DtjSM8z0pz54un",
-//       invoice_settings: {
-//         default_payment_method: "pm_1Oyhd71D08DtjSM8z0pz54un",
-//       },
-//     });
-//     console.log(customers);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// })();
-
-
- * create payment method for customer
- * pass real  visa card number
- * you must use public key not secret
- * pass payment method id to create customer.payment_method
- */
-// (async () => {
-//   try {
-//     const paymentMethod = await stripe.paymentMethods.create({
-//       type: "card",
-//       card: {
-//         number: "4242424242424242", // Test card number, do NOT use real card details
-//         exp_month: 12,
-//         exp_year: 2024,
-//         cvc: "699",
-//       },
-//     });
-//     console.log(paymentMethod);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// })();
-
-/* attach payment method with customer
- * pass visa card id from create payment method id
- * you must use secret key not secret
- */
-
-// (async () => {
-//   try {
-//     const paymentMethod = await stripe.paymentMethods.attach(
-//       "pm_1OyhxX1D08DtjSM8dwRbmlx0",
-//       {
-//         customer: "cus_PnyoH8dZftfpRV",
-//       }
-//     );
-//     console.log(paymentMethod);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// })();
-
-/**
- * here we realy pay  so user select amount and currency
- * then its return payment id that we saved to pass it in payment confirm
- */
-// (async () => {
-//   try {
-//     const intnet = await stripe.paymentIntents.create({
-//       amount: 5486,
-//       currency: "usd",
-//     });
-//     console.log(intnet.id);
-//     console.log(intnet.status);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// })();
-
-// (async () => {
-//   try {
-//     const paymentIntent = await stripe.paymentIntents.confirm(
-//       "pi_3OyiVn1D08DtjSM81K7wqYc7",
-//       {
-//         payment_method: "pm_card_visa",
-//         return_url: "https://www.example.com",
-//       }
-//     );
-//     console.log(paymentIntent.id);
-//     console.log(paymentIntent.status);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// })();
-
 app.post("/confirm-payment", async (req, res) => {
   const { paymentIntentId, paymentMethodId, returnUrl } = req.body;
 
@@ -159,7 +49,6 @@ app.post(
     // Only verify the event if you have an endpoint secret defined.
     // Otherwise use the basic event deserialized with JSON.parse
     const endpointSecret = process.env.END_POIN_SECRET;
-    // Get the signature sent by Stripe
     const signature = req.headers["stripe-signature"];
     try {
       event = stripe.webhooks.constructEvent(
@@ -169,7 +58,7 @@ app.post(
       );
     } catch (err) {
       console.log(`⚠️  Webhook signature verification failed.`, err.message);
-      return response.sendStatus(400);
+      return res.sendStatus(400);
     }
 
     // Handle the event
@@ -179,21 +68,16 @@ app.post(
         console.log(
           `PaymentIntent for ${paymentIntent.amount} was successful!`
         );
-        // Then define and call a method to handle the successful payment intent.
-        // handlePaymentIntentSucceeded(paymentIntent);
+
         break;
       case "payment_method.attached":
         const paymentMethod = event.data.object;
-        // Then define and call a method to handle the successful attachment of a PaymentMethod.
-        // handlePaymentMethodAttached(paymentMethod);
         break;
       default:
-        // Unexpected event type
         console.log(`Unhandled event type ${event.type}.`);
     }
 
-    // Return a 200 response to acknowledge receipt of the event
-    response.send();
+    res.send();
   }
 );
 schedualeDailyUpdateMessage();
