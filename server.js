1d24cdf3ee150f1366aaa4a7cc70ec1cb9709b36
basim jawad <basemjm998@gmail.com>
2024-03-27 03:19:32 +0300

    webhook listener for payment

--- server.js
+++ server.js
@@ -155,8 +155,45 @@ app.post(
   "/payment-webhook",
   express.raw({ type: "application/json" }),
   (req, res) => {
-    const event = JSON.parse(req.body);
-    console.log(event.type);
+    let event = req.body;
+    // Only verify the event if you have an endpoint secret defined.
+    // Otherwise use the basic event deserialized with JSON.parse
+    const endpointSecret = process.env.END_POIN_SECRET;
+    // Get the signature sent by Stripe
+    const signature = req.headers["stripe-signature"];
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.body,
+        signature,
+        endpointSecret
+      );
+    } catch (err) {
+      console.log(`⚠️  Webhook signature verification failed.`, err.message);
+      return response.sendStatus(400);
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntent = event.data.object;
+        console.log(
+          `PaymentIntent for ${paymentIntent.amount} was successful!`
+        );
+        // Then define and call a method to handle the successful payment intent.
+        // handlePaymentIntentSucceeded(paymentIntent);
+        break;
+      case "payment_method.attached":
+        const paymentMethod = event.data.object;
+        // Then define and call a method to handle the successful attachment of a PaymentMethod.
+        // handlePaymentMethodAttached(paymentMethod);
+        break;
+      default:
+        // Unexpected event type
+        console.log(`Unhandled event type ${event.type}.`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
   }
 );
 schedualeDailyUpdateMessage();
