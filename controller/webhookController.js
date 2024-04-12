d46bb2c4ac9b7f42c5e5d204f8737dabce03ed08
basim jawad <basemjm998@gmail.com>
2024-04-12 15:12:07 +0300

    Add error handling fofr post webhook

--- controller/webhookController.js
+++ controller/webhookController.js
@@ -38,37 +38,37 @@ async function aiAnswer(msg_body, phoneNum, next) {
     return dbAnswer;
   }
 
-  // const message = user.chatHistory.flatMap((msg) => [
-  //   {
-  //     role: "user",
-  //     content: msg.userMessage,
-  //   },
-  //   {
-  //     role: "assistant",
-  //     content: msg.aiMessage,
-  //   },
-  // ]);
+  const message = user.chatHistory.flatMap((msg) => [
+    {
+      role: "user",
+      content: msg.userMessage,
+    },
+    {
+      role: "assistant",
+      content: msg.aiMessage,
+    },
+  ]);
 
-  // message.push({ role: "user", content: msg_body });
+  message.push({ role: "user", content: msg_body });
 
-  // const chatCompletion = await openai.chat.completions.create({
-  //   messages: message,
-  //   model: "gpt-3.5-turbo",
-  // });
-  // const aiMessage = chatCompletion.choices[0].message.content;
+  const chatCompletion = await openai.chat.completions.create({
+    messages: message,
+    model: "gpt-3.5-turbo",
+  });
+  const aiMessage = chatCompletion.choices[0].message.content;
 
-  // const newChatHistory = {
-  //   userMessage: msg_body,
-  //   aiMessage: aiMessage,
-  // };
+  const newChatHistory = {
+    userMessage: msg_body,
+    aiMessage: aiMessage,
+  };
 
-  // const newChats = await ChatHistoryModel.create(newChatHistory);
+  const newChats = await ChatHistoryModel.create(newChatHistory);
 
-  // user.chatHistory.push(newChats);
+  user.chatHistory.push(newChats);
 
-  // await user.save();
+  await user.save();
 
-  // return chatCompletion.choices[0].message.content;
+  return chatCompletion.choices[0].message.content;
 }
 
 exports.getWebhookMessage = async (req, res) => {
@@ -88,65 +88,67 @@ exports.getWebhookMessage = async (req, res) => {
 exports.postWeebhook = async (req, res, next) => {
   let body_param = req.body;
 
-  const hasStatuses = body_param.entry.some((entry) =>
-    entry.changes.some((change) => change.value.hasOwnProperty("statuses"))
-  );
+  try {
+    const hasStatuses = body_param.entry.some((entry) =>
+      entry.changes.some((change) => change.value.hasOwnProperty("statuses"))
+    );
 
-  if (hasStatuses) {
-    console.log("The body contains statuses");
-    body_param.entry.forEach(async (entry) => {
-      entry.changes.forEach(async (change) => {
-        change.value.statuses.forEach(async (status) => {
-          if (status.status !== "status") {
-            await updateStatus(status.id, status.status);
+    if (hasStatuses) {
+      console.log("The body contains statuses");
+      for (const entry of body_param.entry) {
+        for (const change of entry.changes) {
+          for (const status of change.value.statuses) {
+            if (status.status !== "status") {
+              await updateStatus(status.id, status.status);
+            }
           }
-        });
-      });
-    });
-  }
-
-  console.log(JSON.stringify(body_param, null, 2));
-
-  if (body_param.object) {
-    console.log("inside body param");
-    if (
-      body_param.entry &&
-      body_param.entry[0].changes &&
-      body_param.entry[0].changes[0].value.messages &&
-      body_param.entry[0].changes[0].value.messages[0]
-    ) {
-      let phon_no_id =
-        body_param.entry[0].changes[0].value.metadata.phone_number_id;
-      let from = body_param.entry[0].changes[0].value.messages[0].from;
-      let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
-
-      console.log("phone number " + phon_no_id);
-      console.log("from " + from);
-      console.log("boady param " + msg_body);
-      await saveNumber(from, phon_no_id, next);
-      let aiMessage = await aiAnswer(msg_body, from, next);
-      axios({
-        method: "POST",
-        url:
-          "https://graph.facebook.com/v13.0/" +
-          phon_no_id +
-          "/messages?access_token=" +
-          token,
-        data: {
-          messaging_product: "whatsapp",
-          to: from,
-          text: {
-            body: aiMessage,
-          },
-        },
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      res.sendStatus(200);
-    } else {
-      res.sendStatus(404);
+        }
+      }
     }
+
+    console.log(JSON.stringify(body_param, null, 2));
+
+    if (body_param.object) {
+      console.log("inside body param");
+      if (
+        body_param.entry &&
+        body_param.entry[0].changes &&
+        body_param.entry[0].changes[0].value.messages &&
+        body_param.entry[0].changes[0].value.messages[0]
+      ) {
+        let phon_no_id =
+          body_param.entry[0].changes[0].value.metadata.phone_number_id;
+        let from = body_param.entry[0].changes[0].value.messages[0].from;
+        let msg_body =
+          body_param.entry[0].changes[0].value.messages[0].text.body;
+
+        console.log("phone number " + phon_no_id);
+        console.log("from " + from);
+        console.log("body param " + msg_body);
+        await saveNumber(from, phon_no_id, next);
+        let aiMessage = await aiAnswer(msg_body, from, next);
+        await axios({
+          method: "POST",
+          url: `https://graph.facebook.com/v13.0/${phon_no_id}/messages?access_token=${process.env.TOKEN}`,
+          data: {
+            messaging_product: "whatsapp",
+            to: from,
+            text: {
+              body: aiMessage,
+            },
+          },
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        res.sendStatus(200);
+      } else {
+        res.sendStatus(404);
+      }
+    }
+  } catch (error) {
+    console.error("Unhandled Error:", error);
+    res.sendStatus(500);
   }
 };
