bb9307b48268fa53d4c6ec2d4d76d12440ca6b0a
basim jawad <basemjm998@gmail.com>
2024-04-12 15:22:04 +0300

    add if to check ai answer message

--- controller/webhookController.js
+++ controller/webhookController.js
@@ -89,66 +89,54 @@ exports.postWeebhook = async (req, res, next) => {
   let body_param = req.body;
 
   try {
-    const hasStatuses = body_param.entry.some((entry) =>
-      entry.changes.some((change) => change.value.hasOwnProperty("statuses"))
-    );
-
-    if (hasStatuses) {
-      console.log("The body contains statuses");
+    // Checking for message presence to process further
+    if (body_param.object && body_param.entry) {
       for (const entry of body_param.entry) {
         for (const change of entry.changes) {
-          for (const status of change.value.statuses) {
-            if (status.status !== "status") {
-              await updateStatus(status.id, status.status);
+          if (change.field === "messages" && change.value.messages) {
+            for (const message of change.value.messages) {
+              const phoneNum = message.from;
+              const msg_body = message.text.body;
+              const phon_no_id =
+                entry.changes[0].value.metadata.phone_number_id;
+
+              console.log("Phone number ID: " + phon_no_id);
+              console.log("From: " + phoneNum);
+              console.log("Message body: " + msg_body);
+
+              await saveNumber(phoneNum, phon_no_id, next);
+              const aiMessage = await aiAnswer(msg_body, phoneNum, next);
+
+              if (aiMessage) {
+                await axios({
+                  method: "POST",
+                  url: `https://graph.facebook.com/v13.0/${phon_no_id}/messages?access_token=${process.env.TOKEN}`,
+                  data: {
+                    messaging_product: "whatsapp",
+                    to: phoneNum,
+                    text: {
+                      body: aiMessage,
+                    },
+                  },
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                });
+                res.sendStatus(200);
+                return;
+              }
             }
           }
         }
       }
     }
 
-    console.log(JSON.stringify(body_param, null, 2));
-
-    if (body_param.object) {
-      console.log("inside body param");
-      if (
-        body_param.entry &&
-        body_param.entry[0].changes &&
-        body_param.entry[0].changes[0].value.messages &&
-        body_param.entry[0].changes[0].value.messages[0]
-      ) {
-        let phon_no_id =
-          body_param.entry[0].changes[0].value.metadata.phone_number_id;
-        let from = body_param.entry[0].changes[0].value.messages[0].from;
-        let msg_body =
-          body_param.entry[0].changes[0].value.messages[0].text.body;
-
-        console.log("phone number " + phon_no_id);
-        console.log("from " + from);
-        console.log("body param " + msg_body);
-        await saveNumber(from, phon_no_id, next);
-        let aiMessage = await aiAnswer(msg_body, from, next);
-        await axios({
-          method: "POST",
-          url: `https://graph.facebook.com/v13.0/${phon_no_id}/messages?access_token=${process.env.TOKEN}`,
-          data: {
-            messaging_product: "whatsapp",
-            to: from,
-            text: {
-              body: aiMessage,
-            },
-          },
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-
-        res.sendStatus(200);
-      } else {
-        res.sendStatus(404);
-      }
-    }
+    res.sendStatus(404);
+    console.error("inside Error message empty:", error);
   } catch (error) {
-    console.error("Unhandled Error:", error);
-    res.sendStatus(500);
+    console.error("outside Error:", error);
+    if (!res.headersSent) {
+      res.sendStatus(500);
+    }
   }
 };
